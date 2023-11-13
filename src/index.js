6e9991fc29fefab2f3f840d0b8c1032511355114
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-11-13 20:39:18 +1300

    Initialise react query

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 import './styles/index.css'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const client = new QueryClient()
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <QueryClientProvider client={client}>
+    <React.StrictMode>
+      <App />
+      <ReactQueryDevtools />
+    </React.StrictMode>
+  </QueryClientProvider>
 )
